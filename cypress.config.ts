5b55bfb8993edbde29c064f674042baed2d01c23
Lape <larape1993@gmail.com>
2023-04-27 12:32:19 +0200

    removed waiton

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://127.0.0.1:3000",
   },
 });
